3eb0b705a8624d9d42be9f163686ede4c10888be
ahmad12-1997 <ahmed.7172@hotmail.com>
2022-11-08 13:38:18 +0300

    simple

--- main.py
+++ main.py
@@ -14,22 +14,22 @@ def page_not_found(e):
 
 main = Flask(__name__)
 ckeditor = CKEditor(main)
-main.config.from_object(config.config['development'])
+#main.config.from_object(config.config['development'])
 main.register_error_handler(404, page_not_found)
 
 
-@main.route('/', methods=["GET", "POST"])
+"""@main.route('/', methods=["GET", "POST"])
 def index():
     return render_template('index.html', **locals())
 
 @main.route('/profile', methods=["GET", "POST"])
 def profile():
-    return render_template('profile.html', **locals())
+    return render_template('profile.html', **locals())"""
 
 
 
 
-@main.route('/content-generator', methods=["GET", "POST"])
+@main.route('/', methods=["GET", "POST"])
 def contentgenerator():
       
     if request.method == 'POST':
@@ -70,4 +70,4 @@ def contentgenerator():
 
 
 if __name__ == '__main__':
-    main.run(host='0.0.0.0', port='5000', debug=True)
+    main.run(host='0.0.0.0', port='5000', debug=False)
